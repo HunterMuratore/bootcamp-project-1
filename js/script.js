ed289122edb19a8e3839b417c2f3e9834fee0afc
MedelsteinE <medelstein1123@gmail.com>
2023-09-20 16:41:13 -0400

    playing around with bulma and modals

---
+++ js/script.js
@@ -0,0 +1,14 @@
+//var modal = $('.modal');
+
+// function openModal() {
+//     $(".modal").show(); // or $("#modal").css("display", "block");
+// }
+
+// // Function to close the modal
+// function closeModal() {
+//     $(".modal").hide(); // or $("#modal").css("display", "none");
+// }
+
+// // Event listeners using jQuery
+// $("#openModalBtn").click(openModal);
+// $("#closeModalBtn").click(closeModal);
